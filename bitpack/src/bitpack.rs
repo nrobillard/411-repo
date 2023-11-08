10ce26b4b9475c3978264ff79487083337b8b6a3
Connor Montague <90429535+cmontague4@users.noreply.github.com>
2023-11-08 12:53:53 -0500

    Add files via upload

---
+++ bitpack/src/bitpack.rs
@@ -0,0 +1,83 @@
+use std::convert::TryInto;
+
+/*
+/// Returns true iff the signed value `n` fits into `width` signed bits.
+/// 
+/// # Arguments:
+/// * `n`: A signed integer value
+/// * `width`: the width of a bit field
+pub fn fitss(n: i64, width: u64) -> bool {
+    false
+}
+
+/// Returns true iff the unsigned value `n` fits into `width` unsigned bits.
+/// 
+/// # Arguments:
+/// * `n`: An usigned integer value
+/// * `width`: the width of a bit field
+pub fn fitsu(n: u64, width: u64) -> bool {
+    false
+}
+
+/// Retrieve a signed value from `word`, represented by `width` bits
+/// beginning at least-significant bit `lsb`.
+/// 
+/// # Arguments:
+/// * `word`: An unsigned word
+/// * `width`: the width of a bit field
+/// * `lsb`: the least-significant bit of the bit field
+pub fn gets(word: u64, width: u64, lsb: u64) -> i64 {
+    0
+}
+
+/// Retrieve an unsigned value from `word`, represented by `width` bits
+/// beginning at least-significant bit `lsb`.
+/// 
+/// # Arguments:
+/// * `word`: An unsigned word
+/// * `width`: the width of a bit field
+/// * `lsb`: the least-significant bit of the bit field
+pub fn getu(word: u64, width: u64, lsb: u64) -> u64 {
+    0
+}
+
+/// Return a modified version of the unsigned `word`,
+/// which has been updated so that the `width` bits beginning at
+/// least-significant bit `lsb` now contain the unsigned `value`.
+/// Returns an `Option` which will be None iff the value does not fit
+/// in `width` unsigned bits.
+/// 
+/// # Arguments:
+/// * `word`: An unsigned word
+/// * `width`: the width of a bit field
+/// * `lsb`: the least-significant bit of the bit field
+/// * `value`: the unsigned value to place into that bit field
+pub fn newu(word: u64, width: u64, lsb: u64, value: u64) -> Option<u64> {
+    Some(0)
+}
+
+/// Return a modified version of the unsigned `word`,
+/// which has been updated so that the `width` bits beginning at
+/// least-significant bit `lsb` now contain the signed `value`.
+/// Returns an `Option` which will be None iff the value does not fit
+/// in `width` signed bits.
+/// 
+/// # Arguments:
+/// * `word`: An unsigned word
+/// * `width`: the width of a bit field
+/// * `lsb`: the least-significant bit of the bit field
+/// * `value`: the signed value to place into that bit field
+pub fn news(word: u64, width: u64, lsb: u64, value: i64) -> Option<u64> {
+    Some(0)
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+*/
